c7d98b3604d4bca6e7383dc5f556cfeb8eb90074
Riccardo <ricky.stucchi@gmail.com>
2014-07-06 17:56:15 +0200

    Added ico of the games and box for the descriptions

---
+++ Code/functionality.js
@@ -0,0 +1,20 @@
+var openDivIndex = -1;
+
+function ShowBox(x){
+	if(openDivIndex != -1){
+		$('#gamebox'+openDivIndex).slideUp( 700, function() { 
+			if(openDivIndex != x)
+				OpenBox(x);
+			else
+				openDivIndex = -1;
+		} );
+	}
+	else{
+		OpenBox(x);
+	}
+}
+
+function OpenBox(x){
+	$('#gamebox'+x).slideDown(700);
+	openDivIndex=x;
+}
