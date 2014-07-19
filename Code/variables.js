451cc43e1860298e767621625cef4a59a9f3516b
Riccardo <ricky.stucchi@gmail.com>
2014-07-19 16:09:38 +0200

    Now every the game has its own popup popolated with the correct imagesgit status

--- Code/variables.js
+++ Code/variables.js
@@ -1,23 +1,21 @@
 // --> Escapalism <--
 var TRAILER_YOTUBE_SRC_ESC = "http://www.youtube.com/embed/voDksRCjEa4";
 
-var IMAGES_SRC_REV = ["Resources/Ecp/0.png","Resources/Ecp/1.png","Resources/Ecp/2.png",
-								"Resources/Ecp/3.png", "Resources/Ecp/4.png"];
+var IMAGES_SRC_REV = ["Resources/Rev/0.png","Resources/Rev/1.png","Resources/Rev/2.png",
+								"Resources/Rev/3.png", "Resources/Rev/4.png"];
 
 var IMAGES_SRC_SMLP = ["Resources/Smlp/0.png","Resources/Smlp/1.png","Resources/Smlp/2.png",
 								"Resources/Smlp/3.png", "Resources/Smlp/4.png", "Resources/Smlp/5.png"];
 
-var IMAGES_SRC_NLW = ["Resources/Ecp/0.png","Resources/Ecp/1.png","Resources/Ecp/2.png",
-								"Resources/Ecp/3.png", "Resources/Ecp/4.png"];
+var IMAGES_SRC_NLW = ["Resources/Nlw/0.png","Resources/Nlw/1.png","Resources/Nlw/2.png",
+								"Resources/Nlw/3.png", "Resources/Nlw/4.png", "Resources/Nlw/5.png", "Resources/Nlw/6.png"];
 
 var IMAGES_SRC_ECP = ["Resources/Ecp/0.png","Resources/Ecp/1.png","Resources/Ecp/2.png",
 								"Resources/Ecp/3.png", "Resources/Ecp/4.png"];
 
-var IMAGES_SRC_SMB = ["Resources/Ecp/0.png","Resources/Ecp/1.png","Resources/Ecp/2.png",
-								"Resources/Ecp/3.png", "Resources/Ecp/4.png"];
+var IMAGES_SRC_SMB = ["Resources/Smb/0.png","Resources/Smb/1.png","Resources/Smb/2.png",
+								"Resources/Smb/3.png", "Resources/Smb/4.png", "Resources/Smb/5.png"];
 
-var IMAGES_SRC_QTM = ["Resources/Ecp/0.png","Resources/Ecp/1.png","Resources/Ecp/2.png",
-								"Resources/Ecp/3.png", "Resources/Ecp/4.png"];
+var IMAGES_SRC_QTM = [ ];
 
-var IMAGES_SRC_THS = ["Resources/Ecp/0.png","Resources/Ecp/1.png","Resources/Ecp/2.png",
-								"Resources/Ecp/3.png", "Resources/Ecp/4.png"];
+var IMAGES_SRC_THS = [ ];
